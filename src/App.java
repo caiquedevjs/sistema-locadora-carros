b716d6860b7429717f841e44eca6e38566908187
Caique Menezes <caiquemenezes1@outlook.com>
2024-02-26 17:34:10 -0300

    new commit

--- src/App.java
+++ src/App.java
@@ -3,12 +3,14 @@ public class App {
         Carro c4 = new Carro();
         c4.nome = "C4";
         c4.ano = 2022;
-        c4.marcha = 5;
         c4.ligar();
 
         Locadora localiza = new Locadora();
         localiza.cadastrarCarros(c4);
-        localiza.consultarCarros();
-
+        c4.desligar();
+        c4.ligar();
+        c4.trocarMarcha();
+        c4.acelerar(50);
+    
     }
 }
--- src/Carro.java
+++ src/Carro.java
@@ -1,8 +1,9 @@
 class Carro {
     public String nome;
     public int ano;
-    public int marcha;
+    private int marcha;
     public boolean ligado;
+    public int velocidade;
 
     public void ligar() {
         if (!this.ligado) {
@@ -19,6 +20,32 @@ class Carro {
             System.out.println("Carro desligado");
         } else {
             System.out.println("Carro já está desligado");
+        }
+    }
+
+   public void acelerar(int velocidade) {
+    if (this.ligado) {
+        if (this.marcha > 1) {
+            if (velocidade > 50) {
+                this.velocidade = velocidade;
+                System.out.println("O carro está andando a " + velocidade + " Km/h");
+            } else {
+                System.out.println("A velocidade mínima para acelerar é de 50 Km/h");
+            }
+        } else {
+            System.out.println("O carro está em marcha lenta. Troque de marcha para acelerar.");
+        }
+    } else {
+        System.out.println("O carro está desligado");
+    }
+}
+
+
+    public void trocarMarcha() {
+        if (this.ligado) {
+            this.marcha += 1;
+        } else {
+            System.out.println("O carro está desligado");
         }
     }
 
